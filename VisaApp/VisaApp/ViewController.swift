fbfa5bf673e8fb02ffadfc51105f1284eb5703dc
Ilya Pugachev <ilyapugachv@gmail.com>
2024-04-06 14:08:42 +0300

    Project created

---
+++ VisaApp/VisaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisaApp
+//
+//  Created by Илья Пугачёв on 6.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
